053309792494db7d00c67b18748eb56fbef34828
kamegoro <tentyo0620@icloud.com>
2023-11-19 21:58:35 +0900

    fix: rename server.go and add gin

---
+++ backend/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"task-canvas/graph"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Note: 参考 https://github.com/99designs/gqlgen/blob/master/docs/content/recipes/gin.md
+func graphqlHandler() gin.HandlerFunc {
+	h := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	return func(ctx *gin.Context) {
+		h.ServeHTTP(ctx.Writer, ctx.Request)
+	}
+}
+
+func playgroundHandler() gin.HandlerFunc {
+	h := playground.Handler("GraphQL", "/query")
+
+	return func(ctx *gin.Context) {
+		h.ServeHTTP(ctx.Writer, ctx.Request)
+	}
+}
+
+func main() {
+	r := gin.Default()
+	r.POST("/query", graphqlHandler())
+	r.GET("/", playgroundHandler())
+	r.Run()
+}
